a9d9a4289211bd9cdf58236955d46f9c0e05e2cd
Jenny-Reinhard <55107673+Jenny-Reinhard@users.noreply.github.com>
2019-10-18 22:44:14 -0700

    initial commit

---
+++ extract.js
@@ -0,0 +1,20 @@
+/* global require */
+/* global module */
+/* global __dirname */
+
+var path = require('path');
+
+var extractFilePath = function(url) {
+  var filePath;
+  var fileName = 'index.html';
+
+  if (url.length > 1) {
+    fileName = url.substring(1);
+  }
+  console.log('The fileName is: ' + fileName);
+
+  filePath = path.resolve(__dirname, 'app', fileName);
+  return filePath;
+};
+
+module.exports = extractFilePath;
